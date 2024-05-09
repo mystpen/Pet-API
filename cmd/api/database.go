0590fa0870daae7cdff1a21f8a50be8f1ae9039e
dokshiye <okshieva@gmail.com>
2024-05-09 19:02:44 +0600

    feat: add database and starting server implementation

---
+++ cmd/api/database.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mystpen/Pet-API/config"
+
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+)
+
+func openDB(cfg config.Config) (*sql.DB, error) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		return nil, errors.Wrap(err, "open sql")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, errors.Wrap(err, "connection is not established")
+	}
+
+	log.Println("Connected to DB")
+
+	return db, nil
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+
+	"github.com/mystpen/Pet-API/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Println(err, nil)
+	}
+
+	// Connect to DB
+	db, err := openDB(*cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//Start api
+	err = Start(cfg)
+	if err != nil{
+		log.Fatal(err)
+	}
 }
